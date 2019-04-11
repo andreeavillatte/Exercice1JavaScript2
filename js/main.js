ba6f1c9629b7f7d1b90058cb31dd148aad39806e
core.editor <andreeavillatte@gmail.com>
2019-04-11 10:37:23 +0200

    ajout d'une function en js

---
+++ js/main.js
@@ -0,0 +1,11 @@
+// cette fonction permet de changer l'image au survol
+function changeImage(){
+    var monImage = document.getElementById('image1');
+    monImage.setAttribute('src' , 'images/image1_2.jpg');
+}
+
+// cette fonction remet l'image d'origine si on ne la survole pas
+// function setBackImage(){
+//     var monImage = document.getElementById('image1');
+//     monImage.setAttribute('src' , 'images/image1.jpg');
+// }
